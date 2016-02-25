7d6108c431e57aec3adfe5d5b35edca0eebe3bb3
Caleb Eby <caleb.eby01@gmail.com>
2016-02-24 18:02:27 -0800

    changes

--- js/main.js
+++ js/main.js
@@ -2,10 +2,10 @@ Waves.attach('.button:not(.flat)', 'waves-light');
 Waves.attach('.button.flat', 'waves-dark');
 Waves.attach('button', 'waves-light');
 Waves.init();
-$(function(){
+$(function() {
   var $textInput = 'input[type="color"], input[type="date"], input[type="datetime"], input[type="datetime-local"], input[type="email"], input[type="month"], input[type="number"], input[type="password"], input[type="search"], input[type="tel"], input[type="text"], input[type="time"], input[type="url"], input[type="week"],textarea';
   $($textInput).each(function() {
-    if ($(this).val() === "" ) {
+    if ($(this).val() === "") {
       $(this).addClass('empty');
     }
   });
@@ -20,3 +20,20 @@ $(function(){
     }
   });
 });
+$.ajax({
+  url: 'https://api.datamuse.com/words?ml=ringing+in+the+ears',
+  type: 'get',
+  dataType: 'json',
+  cache: true,
+  success: function(data) {
+    $(data).each(function(index, value) {
+      console.log(value.word);
+    });
+  }
+});
+
+/*
+$.getJSON('https://api.datamuse.com/words?ml=ringing+in+the+ears', function(data) {
+  console.log(data);
+});
+*/
