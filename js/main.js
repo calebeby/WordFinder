c208aca289b00849a175101d92624707709bca53
Caleb Eby <caleb.eby01@gmail.com>
2016-02-24 18:55:00 -0800

    changes

--- js/main.js
+++ js/main.js
@@ -2,8 +2,9 @@ Waves.attach('.button:not(.flat)', 'waves-light');
 Waves.attach('.button.flat', 'waves-dark');
 Waves.attach('button', 'waves-light');
 Waves.init();
+var currentQuery;
+var $textInput = 'input[type="color"], input[type="date"], input[type="datetime"], input[type="datetime-local"], input[type="email"], input[type="month"], input[type="number"], input[type="password"], input[type="search"], input[type="tel"], input[type="text"], input[type="time"], input[type="url"], input[type="week"],textarea';
 $(function() {
-  var $textInput = 'input[type="color"], input[type="date"], input[type="datetime"], input[type="datetime-local"], input[type="email"], input[type="month"], input[type="number"], input[type="password"], input[type="search"], input[type="tel"], input[type="text"], input[type="time"], input[type="url"], input[type="week"],textarea';
   $($textInput).each(function() {
     if ($(this).val() === "") {
       $(this).addClass('empty');
@@ -20,20 +21,44 @@ $(function() {
     }
   });
 });
-$.ajax({
-  url: 'https://api.datamuse.com/words?ml=ringing+in+the+ears',
-  type: 'get',
-  dataType: 'json',
-  cache: true,
-  success: function(data) {
-    $(data).each(function(index, value) {
-      console.log(value.word);
-    });
+$(".searchbox").on('change keyup keydown paste keypress', function() {
+  if (currentQuery != $(this).val()) {
+    loadWord($(this).val());
   }
 });
-
-/*
-$.getJSON('https://api.datamuse.com/words?ml=ringing+in+the+ears', function(data) {
-  console.log(data);
-});
-*/
+var switchWordTo = function(word) {
+  currentQuery = word;
+  $(".searchbox").val(word);
+  loadWord(word);
+}
+var loadWord = function(query) {
+  currentQuery = query;
+  //similarWords
+  $.ajax({
+    url: 'https://api.datamuse.com/words?ml=' + query +"&max=10",
+    type: 'get',
+    dataType: 'json',
+    cache: true,
+    success: function(data) {
+      var $list = [];
+      $(data).each(function(index, value) {
+        $list.push("<p>"+value.word+"</p>");
+      });
+      $(".similarWords .text").html($list);
+    }
+  });
+  //rhymes
+  $.ajax({
+    url: 'https://api.datamuse.com/words?rel_rhy=' + query.split(" ").pop() +"&max=10",
+    type: 'get',
+    dataType: 'json',
+    cache: true,
+    success: function(data) {
+      var $list = [];
+      $(data).each(function(index, value) {
+        $list.push("<p>"+value.word+"</p>");
+      });
+      $(".rhymes .text").html($list);
+    }
+  });
+};
