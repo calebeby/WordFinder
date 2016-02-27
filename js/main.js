c6c3cc6c7706549d64f40039389b5c311e1b990c
Caleb Eby <caleb.eby01@gmail.com>
2016-02-26 19:34:13 -0800

    changes

--- js/main.js
+++ js/main.js
@@ -21,16 +21,21 @@ $(function() {
     }
   });
 });
-$(".searchbox").on('change keyup keydown paste keypress', function() {
-  if (currentQuery != $(this).val()) {
-    loadWord($(this).val());
-  }
-});
 var switchWordTo = function(word) {
   currentQuery = word;
   $(".searchbox").val(word);
   loadWord(word);
 }
+$.fn.pressEnter = function(fn) {
+  return this.each(function() {
+    $(this).on('enterPress', fn);
+    $(this).keyup(function(e) {
+      if (e.keyCode == 13) {
+        $(this).trigger("enterPress");
+      }
+    })
+  });
+};
 var loadWord = function(query) {
   currentQuery = query;
   //similarWords
@@ -42,7 +47,7 @@ var loadWord = function(query) {
     success: function(data) {
       var $list = [];
       $(data).each(function(index, value) {
-        $list.push("<p>"+value.word+"</p>");
+        $list.push("<p><a href='#"+value.word+"'>"+value.word+"</p>");
       });
       $(".similarWords .text").html($list);
     }
@@ -56,9 +61,33 @@ var loadWord = function(query) {
     success: function(data) {
       var $list = [];
       $(data).each(function(index, value) {
-        $list.push("<p>"+value.word+"</p>");
+        $list.push("<p><a href='#"+value.word+"'>"+value.word+"</p>");
       });
       $(".rhymes .text").html($list);
     }
   });
+  $.ajax({
+    url: 'http://api.wordnik.com:80/v4/word.json/' + query + "/definitions?limit=1&includeRelated=false&sourceDictionaries=century&useCanonical=true&includeTags=false&api_key=a2a73e7b926c924fad7001ca3111acd55af2ffabf50eb4ae5",
+    type: 'get',
+    dataType: 'json',
+    cache: true,
+    success: function(data) {
+      var $list = [];
+      $(data).each(function(index, value) {
+        $list.push("<p>"+value.text+"</p>");
+      });
+      $(".definition .text").html($list);
+    }
+  });
+};
+$(document).ready(function() {
+   switchWordTo(window.location.hash.substring(1));
+});
+$(".searchbox").pressEnter(function() {
+  if (currentQuery != $(this).val()) {
+    loadWord($(this).val());
+  }
+});
+window.onhashchange = function() {
+  switchWordTo(window.location.hash.substring(1));
 };
